ee492ee07c86411cb78c34e7cc4c03c2cd991957
suraj4840 <144480393+suraj4840@users.noreply.github.com>
2023-09-26 19:29:01 +0530

    Update up_down.v

--- up_down.v
+++ up_down.v
@@ -1,1 +1,42 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 7/24/2023 03:41:17 PM
+// Design Name: 
+// Module Name: up_counter_tb
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module up_counter_tb();
+reg clk,rst;
+wire [3:0]count;
+
+up_counter dut(clk,rst,count);
+
+initial 
+begin
+clk =1'b0;
+rst =1'b1;
+#10
+rst =1'b0;
+#200
+$finish;
+end
+
+always #5 clk=~clk;
+
+endmodule
 
